707f88eba5bd8d07ce9bf6a3b3ea35ddd19201ce
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-02-19 23:42:39 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='smart_range',
+    version='1.0.0',
+    packages=['smart_range'],
+    url='https://github.com/PythonCoderAS/SmartRange',
+    license='MIT',
+    author='PythonCoderAS',
+    author_email='sarkaraoyan@gmail.com',
+    description='A class to easily deal with ranges.',
+    extras_require={"docs": ["sphinx"]}
+)
